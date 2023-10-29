7eef1e256a836df2656a9f3bc858dba4fb2e15c0
imen lakrib <lak12imen@gmail.com>
2023-10-29 15:04:31 +0100

    logout

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -163,3 +163,18 @@ export const loginUser = CatchAsyncError(
     }
   }
 );
+
+export const logoutUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.cookie("access_token", "", { maxAge: 1 });
+      res.cookie("refresh_token", "", { maxAge: 1 });
+      res.status(200).json({
+        success: true,
+        message: "Logged out successfully",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -3,6 +3,7 @@ import {
   registrationUser,
   activateUser,
   loginUser,
+  logoutUser,
 } from "../controllers/user.controller";
 
 const useRouter = express.Router();
@@ -10,5 +11,6 @@ const useRouter = express.Router();
 useRouter.post("/registration", registrationUser);
 useRouter.post("/activate-user", activateUser);
 useRouter.post("/login", loginUser);
+useRouter.get("/logout", logoutUser);
 
 export default useRouter;
