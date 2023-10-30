8890365ef5afa18487ebd01a3c1119671eb3b4dd
imen lakrib <lak12imen@gmail.com>
2023-10-30 08:59:36 +0100

    protect route -authenticated user-

---
+++ server/middleware/auth.ts
@@ -0,0 +1,38 @@
+// protect route:
+import { Request, Response, NextFunction } from "express";
+import { CatchAsyncError } from "./catchAsyncErrors";
+import ErrorHandler from "../utils/ErrorHandler";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { redis } from "../utils/redis";
+
+//authenticated user
+export const isAuthenticated = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const access_token = req.cookies.access_token;
+    if (!access_token) {
+      //user not logged in
+      return next(
+        new ErrorHandler("Please login to access this resource", 400)
+      );
+    }
+    const decoded = jwt.verify(
+      access_token,
+      process.env.ACCESS_TOKEN as string
+    ) as JwtPayload;
+
+    if (!decoded) {
+      //not valid
+      return new ErrorHandler("Access token is not valid", 400);
+    }
+
+    //search the user in redis cuz we stored our data in redis cash
+    const user = await redis.get(decoded.id);
+
+    if (!user) {
+      return new ErrorHandler("User not found", 400);
+    }
+    req.user = JSON.parse(user);
+    // here probably user type not defind so craete folder @types to declare his type
+    next();
+  }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -5,12 +5,13 @@ import {
   loginUser,
   logoutUser,
 } from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/auth";
 
 const useRouter = express.Router();
 
 useRouter.post("/registration", registrationUser);
 useRouter.post("/activate-user", activateUser);
 useRouter.post("/login", loginUser);
-useRouter.get("/logout", logoutUser);
+useRouter.get("/logout",isAuthenticated, logoutUser);
 
 export default useRouter;
