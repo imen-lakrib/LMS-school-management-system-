67aecf7fec4e3e891a856cfa7bcdd07b44e898da
imen lakrib <lak12imen@gmail.com>
2023-10-31 09:26:10 +0100

    edit course

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -45,4 +45,42 @@ export const uploadCourse = CatchAsyncError(
   }
 );
 
+// edit course:
 
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      if (thumbnail) {
+        // delete the old thumbnail from cloudinary
+        await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+        // upload the new one
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+        // give it the link
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+
+      // get course id
+      const courseId = req.params.id;
+
+      const course = await CourseModel.findByIdAndUpdate(
+        courseId,
+        { $set: data },
+        { new: true }
+      );
+
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,11 +1,21 @@
 import express from "express";
 
 import { isAuthenticated, authorizeRoles } from "../middleware/auth";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 
 const courseRouter = express.Router();
 //course
 courseRouter.post(
-  "/create-course", isAuthenticated,authorizeRoles("admin"),uploadCourse);
+  "/create-course",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  uploadCourse
+);
+courseRouter.put(
+  "/edit-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editCourse
+);
 
 export default courseRouter;
