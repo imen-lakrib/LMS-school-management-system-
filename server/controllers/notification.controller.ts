409418507c5bbc57cb655d8c1f7f4d80a5cf543f
imen lakrib <lak12imen@gmail.com>
2023-11-02 13:50:53 +0100

    delete notification after each month- cron job-

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import NotificationModel from "../models/notification.model";
-
+import cron from "node-cron";
 //get all notifications --only admin
 export const getNotifications = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -49,5 +49,12 @@ export const updateNotifications = CatchAsyncError(
   }
 );
 
-
 // crond job to delete notification with status read after each month
+cron.schedule("0 0 0 * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  // delete notification:
+  await NotificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+});
