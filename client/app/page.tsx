0c72804778d07e00a9e226e3fe0be658b28d9afa
imen lakrib <lak12imen@gmail.com>
2023-11-05 08:14:10 +0100

    client side

---
+++ client/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React, { FC, useState } from "react"
+
+interface Props { }
+
+const Page: FC<Props> = (props) => {
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Page
