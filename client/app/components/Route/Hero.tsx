379dabc9683bd5297009424c0c54267b36343e3a
imen lakrib <lak12imen@gmail.com>
2023-11-05 13:14:12 +0100

    hero ui

---
+++ client/app/components/Route/Hero.tsx
@@ -0,0 +1,86 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { FC } from "react";
+import { BiSearch } from "react-icons/bi";
+type Props = {};
+const Hero: FC<Props> = (props) => {
+  return (
+    <div className="w-full 1000px:flex items-center">
+      <div className="absolute top-[100px] 1000px:top-[unset]  1500px:h-[700px] 1500px:w-[700px] xl:h-[600px] 1100px:w-[600px] h-[50vh] w-[50vh] hero_animation"></div>
+      <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-70px 1000px:pt-[0] z-10">
+        <Image
+          src="/assets/download.jpeg"
+          width={90}
+          height={500}
+          alt=""
+          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
+        />
+      </div>
+
+      <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
+        <h2 className="dark:text-white test-[#000000c7]  text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin py-2 1000px:leading-[75px] ">
+          Improve Your Online Learning Experience Better Instantly
+        </h2>
+        <br />
+        <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]">
+          We have 48k+ Online courses & 500K+ Online registered students. Find
+          your desired courses from them.
+        </p>
+        <br />
+        <br />
+        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
+          <input
+            type="search"
+            placeholder="Search Courses..."
+            className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder-text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin "
+          />
+          <div className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]">
+            <BiSearch className="text-white" size={30} />
+          </div>
+        </div>
+        <br />
+        <br />
+
+        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center">
+          <Image
+            src="/assets/download.jpeg"
+            width={60}
+            height={60}
+            alt="Your Alt Text"
+            className="rounded-full "
+          />
+
+          <Image
+            src="/assets/download.jpeg"
+            width={60}
+            height={60}
+            alt="Your Alt Text"
+            className="rounded-full ml-[-20px]"
+          />
+
+          <Image
+            src="/assets/download.jpeg"
+            width={60}
+            height={60}
+            alt="Your Alt Text"
+            className="rounded-full ml-[-20px]"
+          />
+
+          <p className="font-Josefin dark: text-[#edfff4] text-[#000000b3] 1000px:pl-3 text-[18px] font-[600]">
+            500k+ People already trusted us.{""}
+            <Link
+              href="/courses"
+              className="dark:text-[#46e256] text-[crimson]"
+            >
+              View Courses
+            </Link>
+          </p>
+        </div>
+
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,6 +2,7 @@
 import React, { FC, useState } from "react";
 import Heading from "./utils/Heading";
 import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
 interface Props {}
 
 const Page: FC<Props> = (props) => {
@@ -16,6 +17,7 @@ const Page: FC<Props> = (props) => {
         keywords="school, management, system"
       />
       <Header open={open} setOpen={setOpen} activeItem={activeItem} />
+      <Hero />
     </div>
   );
 };
