68e08cb724fe3d4e104308322a9dbdccd9caf9fe
imen lakrib <lak12imen@gmail.com>
2023-11-07 12:03:50 +0100

    loader

---
+++ client/app/components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Loader.css";
+
+export const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="loader"></div>
+    </div>
+  );
+};
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -6,6 +6,9 @@ import { ThemeProvider } from "./utils/theme-provider";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
 import { SessionProvider } from "next-auth/react";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
+import { Loader } from "./components/Loader/Loader";
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -31,8 +34,8 @@ export default function RootLayout({
         <Providers>
           <SessionProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-              {" "}
-              {children}
+              <Custom> {children}</Custom>
+
               <Toaster position="top-center" reverseOrder={false} />
             </ThemeProvider>
           </SessionProvider>
@@ -41,3 +44,8 @@ export default function RootLayout({
     </html>
   );
 }
+
+const Custom: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { isLoading } = useLoadUserQuery({});
+  return <>{isLoading ? <Loader /> : <>{children}</>}</>;
+};
