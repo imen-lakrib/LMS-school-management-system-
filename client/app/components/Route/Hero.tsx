4761334d044674340bc51b5ef4a3cdf2aee3e12d
imen lakrib <lak12imen@gmail.com>
2023-12-03 09:15:37 +0100

    fix loading in home page

--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -4,85 +4,92 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { FC } from "react";
 import { BiSearch } from "react-icons/bi";
+import { Loader } from "../Loader/Loader";
 type Props = {};
 const Hero: FC<Props> = (props) => {
-  const { data } = useGetLayoutQuery("Banner", {});
+  const { data, isLoading } = useGetLayoutQuery("Banner", {});
   return (
-    <div className="w-full 1000px:flex items-center">
-      <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-center pt-[70px] 1000px:pt-[0] z-10 relative ">
-        <Image
-          src={data?.layout?.banner?.image?.url}
-          width={400}
-          height={400}
-          alt=""
-          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
-        />
-        <div className="absolute top-[100px]  1000px:top-[unset]  1500px:h-[700px] 1500px:w-[700px] 1100px:h-[600px] 1100px:w-[600px] h-[40vh] w-[40vh] hero_animation rounded-full"></div>
-      </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="w-full 1000px:flex items-center">
+          <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-center pt-[70px] 1000px:pt-[0] z-10 relative ">
+            <Image
+              src={data?.layout?.banner?.image?.url}
+              width={400}
+              height={400}
+              alt=""
+              className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
+            />
+            <div className="absolute top-[100px]  1000px:top-[unset]  1500px:h-[700px] 1500px:w-[700px] 1100px:h-[600px] 1100px:w-[600px] h-[40vh] w-[40vh] hero_animation rounded-full"></div>
+          </div>
 
-      <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
-        <h2
-          className={`${styles.textColorWhite}  text-[30px]  w-full 1000px:text-[60px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:!w-[55%] 1100px:!w-[78%]`}
-        >
-          {data?.layout?.banner?.title}{" "}
-        </h2>
-        <br />
+          <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
+            <h2
+              className={`${styles.textColorWhite}  text-[30px]  w-full 1000px:text-[60px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:!w-[55%] 1100px:!w-[78%]`}
+            >
+              {data?.layout?.banner?.title}{" "}
+            </h2>
+            <br />
 
-        <p
-          className={`${styles.textColorWhite} font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]`}
-        >
-          {data?.layout?.banner?.subTitle}{" "}
-        </p>
-        <br />
-        <br />
-        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
-          <input
-            type="search"
-            placeholder="Search Courses..."
-            className={`bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder-text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none ${styles.textColorWhite} text-[20px] font-[500] font-Josefin `}
-          />
-          <div className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]">
-            <BiSearch className="text-white" size={30} />
+            <p
+              className={`${styles.textColorWhite} font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]`}
+            >
+              {data?.layout?.banner?.subTitle}{" "}
+            </p>
+            <br />
+            <br />
+            <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50px] bg-transparent relative">
+              <input
+                type="search"
+                placeholder="Search Courses..."
+                className={`bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder-text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none ${styles.textColorWhite} text-[20px] font-[500] font-Josefin `}
+              />
+              <div className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]">
+                <BiSearch className="text-white" size={30} />
+              </div>
+            </div>
+            <br />
+            <br />
+
+            <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center">
+              <Image
+                src={require("../../../public/assets/img.png")}
+                alt="Your Alt Text"
+                className="rounded-full   1500px:w-[12] 1100px:w-[8] w-[60px]"
+              />
+
+              <Image
+                src={require("../../../public/assets/img.png")}
+                alt="Your Alt Text"
+                className="rounded-full  ml-[-20px]  1500px:w-[12] 1100px:w-[8] w-[60px] "
+              />
+
+              <Image
+                src={require("../../../public/assets/img.png")}
+                alt="Your Alt Text"
+                className="rounded-full  ml-[-20px]  1500px:w-[12] 1100px:w-[8] w-[60px] "
+              />
+
+              <p
+                className={`font-Josefin dark: ${styles.textColorWhite} 1000px:pl-3 text-[18px] font-[600]`}
+              >
+                500k+ People already trusted us.{""}
+                <Link
+                  href="/courses"
+                  className="dark:text-[#46e256] text-[crimson]"
+                >
+                  View Courses
+                </Link>
+              </p>
+            </div>
+
+            <br />
           </div>
         </div>
-        <br />
-        <br />
-
-        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] flex items-center">
-          <Image
-            src={require("../../../public/assets/img.png")}
-            alt="Your Alt Text"
-            className="rounded-full   1500px:w-[12] 1100px:w-[8] w-[60px]"
-          />
-
-          <Image
-            src={require("../../../public/assets/img.png")}
-            alt="Your Alt Text"
-            className="rounded-full  ml-[-20px]  1500px:w-[12] 1100px:w-[8] w-[60px] "
-          />
-
-          <Image
-            src={require("../../../public/assets/img.png")}
-            alt="Your Alt Text"
-            className="rounded-full  ml-[-20px]  1500px:w-[12] 1100px:w-[8] w-[60px] "
-          />
-
-          <p
-            className={`font-Josefin dark: ${styles.textColorWhite} 1000px:pl-3 text-[18px] font-[600]`}
-          >
-            500k+ People already trusted us.{""}
-            <Link
-              href="/courses"
-              className="dark:text-[#46e256] text-[crimson]"
-            >
-              View Courses
-            </Link>
-          </p>
-        </div>
-
-        <br />
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
