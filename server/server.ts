ff860a2592fcc7d7bf46f8ba9d86d1f24533c99b
imen lakrib <lak12imen@gmail.com>
2023-12-03 13:41:22 +0100

    fix

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import { app } from "./app";
+import { initSocketServer } from "./socketServer";
 import connectDB from "./utils/db";
 import { v2 as cloudinary } from "cloudinary";
 import http from "http";
@@ -12,6 +13,8 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_SECRET_KEY,
 });
+
+initSocketServer(server);
 
 // create server
 server.listen(process.env.PORT, () => {
