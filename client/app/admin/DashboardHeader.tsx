cec76f8e4b83eef2834e85e18f4955f598a0c8be
imen lakrib <lak12imen@gmail.com>
2023-12-03 15:26:17 +0100

    dynamic notification in admin dashboard

--- client/app/admin/DashboardHeader.tsx
+++ client/app/admin/DashboardHeader.tsx
@@ -1,9 +1,14 @@
 "use client";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import ThemeSwitcher from "../utils/ThemeSwitcher";
 import { NotificationImportant } from "@mui/icons-material";
 //socket
 import socketIO from "socket.io-client";
+import {
+  useGetAllNotificationsQuery,
+  useUpdateNotificationStatusMutation,
+} from "@/redux/features/notifications/notificationApi";
+import { format } from "timeago.js";
 const ENDPOINT = process.env.NEXT_PUBLICK_SOCKET_SERVER_URI || "";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 type Props = {
@@ -12,6 +17,44 @@ type Props = {
 };
 
 const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
+  const { data, refetch } = useGetAllNotificationsQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+  });
+
+  const [updateNotificationStatus, { isSuccess }] =
+    useUpdateNotificationStatusMutation();
+
+  const [notifications, setNotifications] = useState<any>([]);
+
+  const [audio] = useState(new Audio(""));
+
+  const playerNotificationSound = () => {
+    audio.play();
+  };
+
+  useEffect(() => {
+    if (data) {
+      setNotifications(
+        data.notifications.filter((item: any) => item.status === "unread")
+      );
+    }
+    if (isSuccess) {
+      refetch();
+    }
+    audio.load();
+  }, [data, isSuccess]);
+
+  useEffect(() => {
+    socketId.on("newNotification", (data) => {
+      refetch();
+      playerNotificationSound();
+    });
+  }, []);
+
+  const handleNotificationStatusChange = async (id: string) => {
+    await updateNotificationStatus(id);
+  
+
   return (
     <div className="w-full flex items-center justify-end p-6 fixed top-5 right-0">
       <ThemeSwitcher />
@@ -21,7 +64,7 @@ const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
       >
         <NotificationImportant className="text-2xl cursor-pointer dark:text-white text-black" />
         <span className="absolute -top-2 -ri-2 bg-[#3ccba0] rounded-full w-[20px] h-[20px] text-[12px] flex items-center justify-center text-white">
-          3
+          {notifications && notifications.length}
         </span>
       </div>
 
@@ -30,24 +73,27 @@ const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
           <h5 className="text-center text-[20px] font-Poppins text-black dark:text-white p-3">
             Notifications
           </h5>
+          {notifications && notifications.map((item:any, index:any)=>(
+  <div className="dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
+    <div className=" w-full flex items-center justify-between p-2">
+      <p className=" text-black dark:text-white ">
+      {item.title}
 
-          <div className="dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
-            <div className=" w-full flex items-center justify-between p-2">
-              <p className=" text-black dark:text-white ">
-                New Question Received
-              </p>
-              <p className="text-black dark:text-white cursor-pointer ">
-                Mark as read
-              </p>
-            </div>
-            <p className=" px-2 text-black dark:text-white">
-              ggggggggggg gggggggg ggggggggggggg ggggggggggg
-            </p>
+      </p>
+      <p className="text-black dark:text-white cursor-pointer ">
+        Mark as read
+      </p>
+    </div>
+    <p className=" px-2 text-black dark:text-white">
+    {item.message}
+    </p>
 
-            <p className=" p-2 text-black dark:text-white text-[14px] ">
-              5 days ago
-            </p>
-          </div>
+    <p className=" p-2 text-black dark:text-white text-[14px] ">
+    {format(item.createdAt)}
+    </p>
+</div>
+))}
+          
         </div>
       )}
     </div>
